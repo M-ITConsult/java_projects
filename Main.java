ef496b1a76618dbfb73005717d832253cd7760df
M-ITConsult <jchoro74@gmail.com>
2023-08-03 19:00:17 +0200

    update

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        int maVariableEntiere;
+        double maVariableReel;
+        String maVariableChaineCaracteres;
+        maVariableEntiere = 14;
+        maVariableChaineCaracteres = "Hello World!";
+        maVariableReel = 24.58;
+
+        System.out.println(maVariableEntiere);
+        System.out.println(maVariableChaineCaracteres);
+        System.out.println(maVariableReel);
+    }
+}
