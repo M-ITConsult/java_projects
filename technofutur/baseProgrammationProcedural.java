a27d1156ec1cd9d73e1024fe7ac32de88f864e5d
M-ITConsult <jchoro74@gmail.com>
2023-08-08 11:25:55 +0200

    first commit

---
+++ technofutur/baseProgrammationProcedural.java
@@ -0,0 +1,14 @@
+package technofutur;
+
+public class baseProgrammationProcedural {
+
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 4;
+        double c = 5.91;
+        int resultAB = a + b;
+        double resultAC = a - c;
+        final boolean resultFinal = resultAB > resultAC;
+        System.out.println(resultFinal);
+    }
+}
