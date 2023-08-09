4a62b30f8ad339980a602f994112ab582adde5b4
M-ITConsult <jchoro74@gmail.com>
2023-08-09 19:22:47 +0200

    update

---
+++ technofutur/FiveAlgorithm.java
@@ -0,0 +1,61 @@
+package technofutur;
+
+public class FiveAlgorithm {
+
+    public static void main(String[] args) {
+        // Algorithm 1
+//        int a = 0;
+//        int b = 0;
+//
+//        while (a<=10) {
+//            System.out.println(a);
+//            System.out.println(b);
+//            a++;
+//            b++;
+//        }
+
+        // Algorithm 2
+//        int a = 0;
+//        int b = 0;
+//
+//        while (a<10) {
+//            System.out.println(a);
+//            System.out.println(b);
+//            a++;
+//            b++;
+//        }
+
+        // Algorithm 3
+//        int a = 1;
+//        int b = 0;
+//
+//        while (a<=10) {
+//            System.out.println(a);
+//            System.out.println(b);
+//            a++;
+//            b++;
+//        }
+
+        // Algorithm 4
+//        int a = 0;
+//        int b = 0;
+//
+//        while (a<=10) {
+//            System.out.println(a);
+//            System.out.println(b);
+//            a=1;
+//            b=a+b;
+//        }
+
+        // Algorithm 5
+        int a = 10;
+        int b = 0;
+
+        while (a<10) {
+            System.out.println(a);
+            System.out.println(b);
+            a++;
+            b++;
+        }
+    }
+}
--- technofutur/Main.java
+++ technofutur/Main.java
@@ -1,17 +1,41 @@
 package technofutur;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+//    public static void main(String[] args){
+//
+//        int maVariableEntiere;
+//        double maVariableReel;
+//        String maVariableChaineCaracteres;
+//        maVariableEntiere = 14;
+//        maVariableChaineCaracteres = "Hello World!";
+//        maVariableReel = 24.58;
+//
+//        System.out.println(maVariableEntiere);
+//        System.out.println(maVariableChaineCaracteres);
+//        System.out.println(maVariableReel);
 
-        int maVariableEntiere;
-        double maVariableReel;
-        String maVariableChaineCaracteres;
-        maVariableEntiere = 14;
-        maVariableChaineCaracteres = "Hello World!";
-        maVariableReel = 24.58;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(maVariableEntiere);
-        System.out.println(maVariableChaineCaracteres);
-        System.out.println(maVariableReel);
+        final String INSTRUCTION_STOP = "STOP";
+        int nombre, somme = 0;
+        String chaineUtilisateur;
+        boolean continuer = true;
+        System.out.print("Voici votre calculatrice d'addition ! \n Entrer la chaîne 'STOP' pour sortir du programme et ainsi afficher la somme des nombres préalablement entrés. \n");
+        do {
+            System.out.print("Entrer un nombre entier : ");
+            chaineUtilisateur = String.valueOf(scanner.next());
+            if(chaineUtilisateur.equals(INSTRUCTION_STOP)){
+                continuer=false;
+            }
+            else{
+                nombre = Integer.parseInt(chaineUtilisateur);
+                somme+=nombre;
+            }
+        } while (continuer);
+
+        System.out.println("La somme des nombres encodés vaut : " + somme);
     }
 }
