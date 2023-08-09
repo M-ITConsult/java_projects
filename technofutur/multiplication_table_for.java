d6c6c47012141d6979dee21ee1aaaf0fbc4dd59b
M-ITConsult <jchoro74@gmail.com>
2023-08-09 16:26:55 +0200

    update

---
+++ technofutur/multiplication_table_for.java
@@ -0,0 +1,29 @@
+package technofutur;
+
+import java.util.Scanner;
+
+public class multiplication_table_for {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nombre = 0;
+
+        do {
+            System.out.println("Veuillez entrer un nombre positif et non nul: ");
+            while (!scanner.hasNextInt()) {
+                    System.out.println("Erreur: Veuillez entrer un nombre entier positif et non nul.");
+                    scanner.next();
+                }
+            nombre = scanner.nextInt();
+            }  while (nombre <= 0);
+
+
+        System.out.println("Table de multiplication par le nombre " + nombre + " :");
+
+        for (int multiplicateur = 1; multiplicateur <=10; multiplicateur++) {
+            System.out.println(nombre + " x " + multiplicateur + " = " + (nombre * multiplicateur));
+        }
+    }
+}
---
+++ technofutur/multiplication_table_while.java
@@ -0,0 +1,37 @@
+package technofutur;
+
+import java.util.Scanner;
+
+public class multiplication_table_while {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nombre = 0;
+        boolean valide = false;
+
+        while (!valide) {
+            System.out.println("Veuillez entrer un nombre positif et non nul: ");
+            if (scanner.hasNext()) {
+                nombre = scanner.nextInt();
+                if (nombre > 0) {
+                    valide = true;
+                } else {
+                    System.out.println("Erreur: Veuillez entrer un nombre entier positif et non nul.");
+                }
+            } else {
+                System.out.println("Erreur: Veuillez entrer un nombre entier positif et non nul.");
+                scanner.next(); // Consommer l'entrée valide
+            }
+        }
+
+        System.out.println("Table de multiplication par le nombre " + nombre + " :");
+
+        int multiplicateur = 1;
+        while (multiplicateur <= 10) {
+            System.out.println(nombre + " x " + multiplicateur + " = " + (nombre * multiplicateur));
+            multiplicateur++;
+        }
+    }
+}
