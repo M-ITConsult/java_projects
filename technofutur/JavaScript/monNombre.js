3766fe21193d20cec4cb0bbb6316b5d8efc948f3
M-ITConsult <jchoro74@gmail.com>
2023-08-16 15:07:15 +0200

    update

--- technofutur/JavaScript/monNombre.js
+++ technofutur/JavaScript/monNombre.js
@@ -2,8 +2,10 @@ let monNombre = 42 //Affichera "number" dans la console
 let myFirstText = "Hello there!" // Utilisation des guillemets
 let mySecondText = 'Hello there!' // Utilisation de l'apostrophe
 let myThirdText = 'Hello there! It\'s me' // Utilisation de l'antislash
+let myBoolean = true // Type boolean
 
 console.log(typeof monNombre) 
 console.log(typeof myFirstText)
 console.log(typeof mySecondText)
 console.log(typeof myThirdText)
+console.log(typeof myBoolean) 
