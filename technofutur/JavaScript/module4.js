778b91f2ef8614cf7384425cac74eb48316e9311
M-ITConsult <jchoro74@gmail.com>
2023-08-17 10:23:11 +0200

    update

--- technofutur/JavaScript/module4.js
+++ technofutur/JavaScript/module4.js
@@ -5,4 +5,10 @@ function afficherMessage() {
 afficherMessage();
 
 /*------------------------------------------------------------------------------*/
+function calcul(nb1, nb2){
+    diff = nb1 - nb2
+    return diff
+}
 
+const result = calcul(5,3)
+console.log(result)
