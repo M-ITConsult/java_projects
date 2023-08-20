f968b5933feae7185d389a978b1090a54d5794e9
M-ITConsult <jchoro74@gmail.com>
2023-08-20 09:01:14 +0200

    update

--- technofutur/Java/Instruction_Base_Ex_1_TVA.java
+++ technofutur/Java/Instruction_Base_Ex_1_TVA.java
@@ -8,7 +8,7 @@ public class Instruction_Base_Ex_1_TVA {
             System.out.println("Veuillez entrez le prix HTVA: ");
             double prixHTVA = scanner.nextDouble();
             double prixTVAC = prixHTVA * (1 + (TAUX_TVA / 100));
-            // Format du System.out avec System.out.printf >
+            // Format du System.out avec System.out.printf %.2f pour deux chiffres après la virgule>
             System.out.printf("Prix HTVA: %.2f€\nTVA: %.2f%%\nTotal TVAC: %.2f€%n",prixHTVA, TAUX_TVA, prixTVAC);
 
             // Format du System.out sans System.out.printf >
--- technofutur/Java/Structure_conditionnelle_Annees_Bissextiles_Ex_3_test_1.java
+++ technofutur/Java/Structure_conditionnelle_Annees_Bissextiles_Ex_3_test_1.java
@@ -1,4 +1,4 @@
-package technofutur;
+package technofutur.Java;
 
 import java.util.Scanner;
 
@@ -8,12 +8,12 @@ public class Structure_conditionnelle_Annees_Bissextiles_Ex_3_test_1 {
 
         System.out.println("Entrez une année: ");
         Scanner year = new Scanner(System.in);
-        int yearInput = year.nextInt();
+        int yearInput = Integer.parseInt(year.nextLine());
 
         if ((yearInput % 4 == 0 && yearInput % 100 != 0) || yearInput % 400 == 0) {
-            System.out.println(yearInput + " est une année bissextile!");
-        } else {
-            System.out.println(yearInput + " n'est pas une année bissextile!");
+            // %d pour Int %f pour Float
+            System.out.printf("%d est une année bissextile!",yearInput);
+        } else
+            System.out.printf("%d n'est pas une année bissextile!", yearInput);
         }
     }
-}
