6ffc0f0694174680d4eef844671d71ad4135f299
M-ITConsult <jchoro74@gmail.com>
2023-09-11 09:05:08 +0200

    update

--- PDFSplitter.java
+++ PDFSplitter.java
@@ -2,10 +2,15 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import java.io.IOException;
+import java.util.Scanner;
+
 public class PDFSplitter {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // Load the PDF file
-        try (PDDocument document = Loader.loadPDF(new RandomAccessReadBufferedFile("test.pdf") {
+        System.out.print("Input the file name in .pdf: ");
+        String pdfFile = sc.next();
+        try (PDDocument document = Loader.loadPDF(new RandomAccessReadBufferedFile(pdfFile) {
         })){
             // Get the total number of pages
             int totalPages = document.getNumberOfPages();
