6b67e6ea44f660c49724e8041ddf72c768462e16
M-ITConsult <jchoro74@gmail.com>
2023-10-05 17:17:16 +0200

    update

--- technofutur/Java/Stream/Main.java
+++ technofutur/Java/Stream/Main.java
@@ -5,21 +5,26 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> noms = Arrays.asList("");
+        // Création de la liste
+        List<String> noms = List.of("test","test","test","marine","meli","marrion");
 
+        // Filtre de la liste commençant par "m"
         List<String> nomsFiltre = noms.stream()
-                .filter(nom -> nom.startsWith("M"))
-                .collect(Collectors.toList());
+                .filter(nom -> nom.startsWith("m"))
+                .toList();
 
+        // Affichage
         for(String nom : nomsFiltre) {
             System.out.println(nom);
         }
 
+        // Liste en majuscule
         List<String> nomsMaj = noms.stream()
                 //valeur -> valeur.
                 .map(String::toUpperCase)
-                .collect(Collectors.toList());
+                .toList();
 
+        // Affichage
         for(String nom : nomsMaj) {
             System.out.println(nom);
         }
